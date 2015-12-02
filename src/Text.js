ffc2c0a9100e3afa431506e6f3fe3e0573578380
ghaiklor <ghaiklor@gmail.com>
2015-12-02 17:52:43 +0200

    feat(shape): Initial release of the Text shape

---
+++ src/Text.js
@@ -0,0 +1,41 @@
+import Shape from 'kittik-shape-basic';
+
+/**
+ * Implements Text shape which is rendering the text at specified point.
+ *
+ * @since 1.0.0
+ * @version 1.0.0
+ */
+export default class Text extends Shape {
+  /**
+   * Creates new Text instance.
+   *
+   * @constructor
+   * @param {Object} [options] Options go into {@link Shape} class and they are the same.
+   */
+  constructor(options) {
+    super(options);
+  }
+
+  /**
+   * Renders a shape.
+   *
+   * @override
+   * @param {Cursor} cursor
+   * @returns {Text}
+   */
+  render(cursor) {
+    let text = this.getText();
+    let {x, y} = this.getPosition();
+    let background = this.getBackground();
+    let foreground = this.getForeground();
+
+    if (background) cursor.background(background);
+    if (foreground) cursor.foreground(foreground);
+
+    cursor.setPosition(x, y);
+    text.split('').forEach(cursor.write.bind(cursor));
+
+    return this;
+  }
+}
