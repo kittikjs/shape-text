9c27e10d3688a79a65eaa1b5b59f29a4409808ac
ghaiklor <ghaiklor@gmail.com>
2015-12-15 16:03:43 +0200

    fix(shape): Migrate to new Basic shape package

--- src/Text.js
+++ src/Text.js
@@ -4,19 +4,19 @@ import Shape from 'kittik-shape-basic';
  * Implements Text shape which is rendering the text at specified point.
  *
  * @since 1.0.0
- * @version 1.0.0
  */
 export default class Text extends Shape {
   render(cursor) {
     let text = this.getText();
-    let {x, y} = this.getPosition();
+    let x = this.getX();
+    let y = this.getY();
     let background = this.getBackground();
     let foreground = this.getForeground();
 
-    if (background) cursor.background(background);
-    if (foreground) cursor.foreground(foreground);
+    if (background !== undefined) cursor.background(background);
+    if (foreground !== undefined) cursor.foreground(foreground);
 
-    cursor.setPosition(x, y).write(text);
+    cursor.moveTo(x, y).write(text);
 
     return this;
   }
