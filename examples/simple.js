38e29be62e00b9c432182cc973e2d72b80c02e6d
ghaiklor <ghaiklor@gmail.com>
2016-03-27 13:24:23 +0300

    docs(shape): Update examples

--- examples/simple.js
+++ examples/simple.js
@@ -1,11 +1,11 @@
 "use strict";
 
 const Text = require('../lib/Text');
-const cursor = require('kittik-cursor').create().resetTTY();
+const cursor = require('kittik-cursor').create().reset();
 
-Text.create({text: 'Hello, there!', x: 'center', y: 5, underlined: true}).render(cursor);
-Text.create({text: 'It is a simple text shape', x: 'center', y: 7, bold: true}).render(cursor);
-Text.create({text: 'If you see this, it works', x: 'center', y: 9, reverse: true}).render(cursor);
-Text.create({text: 'Also\nyou\ncan\nwrite\nmulti-lines', x: 'center', align: 'right', y: 15, dim: true}).render(cursor);
+Text.create(cursor, {text: 'Hello, there!', x: 'center', y: 5, underlined: true}).render();
+Text.create(cursor, {text: 'It is a simple text shape', x: 'center', y: 7, bold: true}).render();
+Text.create(cursor, {text: 'If you see this, it works', x: 'center', y: 9, reverse: true}).render();
+Text.create(cursor, {text: 'You\ncan\nwrite\nmulti-lines', x: 'center', align: 'right', y: 15, dim: true}).render();
 
 cursor.flush();
